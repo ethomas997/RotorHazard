1e1fc0c094c9c96d159d4b2d38a148dfb1091f1e
E Thomas <ethomas997@etsphere.com>
2026-08-15 02:49:57 -0400

    Add optional requires_auth to RHAPI/RHUI socket_listen for plugin-registered handlers

--- src/server/RHAPI.py
+++ src/server/RHAPI.py
@@ -3,7 +3,7 @@ import functools
 from Database import LapSource
 
 API_VERSION_MAJOR = 1
-API_VERSION_MINOR = 4
+API_VERSION_MINOR = 5
 
 import dataclasses
 import json
@@ -101,8 +101,15 @@ class UserInterfaceAPI():
         self._racecontext.rhui.emit_clear_priority_messages()
 
     # Socket
-    def socket_listen(self, message, handler):
-        self._racecontext.rhui.socket_listen(message, handler)
+    def socket_listen(self, message, handler, requires_auth=False):
+        '''Registers a SocketIO event handler.
+
+        :param requires_auth: If True, the handler is only invoked for
+        clients that have passed the server's admin authentication (the
+        same check used by the server's own destructive/config-mutating
+        SocketIO handlers).
+        '''
+        self._racecontext.rhui.socket_listen(message, handler, requires_auth)
 
     def socket_send(self, message, data):
         self._racecontext.rhui.socket_send(message, data)
